d1497fafa46b43d28348f0a17aedc1d296f3c873
Vithor Varela <vithor.varela@going2.com.br>
2024-06-05 17:55:18 -0300

    initial commit

---
+++ server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
